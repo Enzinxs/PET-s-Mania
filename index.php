6383a79517cc14da78aaa0fb250dc1268033991e
Enzo da Silva Maximiniano <enzo.aluno680@gmail.com>
2026-06-30 10:38:15 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet's Mania</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+</head>
+<body class="has-background-light">
+    <nav class="navbar is-warning" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <figure class="image is-128x128">
+  <img src="./img/pets mania logo.png" class="is-rounded"/>
+</figure>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        <p class="is-size-4"><strong>HOME</strong></p>
+      </a>
+
+      <a class="navbar-item">
+         <p class="is-size-4"><strong>RAÇÕES</strong></p>
+      </a>
+
+      <a class="navbar-item">
+         <p class="is-size-4"><strong>CONTATO</strong></p>
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+    <a class="navbar-link">
+       <p class="is-size-4"><strong>PETs</strong></p>
+    </a>
+
+    <div class="navbar-dropdown">
+      <a class="navbar-item">
+        CÃES
+      </a>
+      <a class="navbar-item">
+        GATOS
+      </a>
+      <a class="navbar-item">
+        AVES
+    <div class="navbar-end">
+      <div class="navbar-item">
+      </div>
+      </div>
+    </div>
+</nav>
+</body>
+</html>
